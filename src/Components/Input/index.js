52066b27572351ae82b95857762168df8e40b112
Leveditor <leveditorfc@gmail.com>
2022-11-19 14:38:24 -0300

    fix: responsive form zip-code

--- src/Components/Input/index.js
+++ src/Components/Input/index.js
@@ -18,42 +18,42 @@ export default function Input() {
 
   return (
     <>
-    <div className="container mx-auto mt-5 border border-gray-400 w-3/5 rounded">
+    <div className="container mx-auto mt-5 border border-gray-400 md:w-3/5  w-10/12 rounded">
       <div className="p-3 mb-5 text-center mb-2 bg-gray-400">
         <p className="text-white"><i className="fas fa-home p-1"></i>Endereço</p>
       </div>
       <hr></hr>
-        <form onSubmit={handleSubmit()} className="ml-5 p-5" >
-          <div className="md:flex justify-center mr-8">
-            <div className="md:ml-6">
+        <form onSubmit={handleSubmit()} className="md:ml-5 p-5" >
+          <div className="md:grid grid-cols-2 gap-4">
+            <div>
               <p>CEP *</p>
-              <input type="text" {...register("cep")} onBlur={checkCEP} className="p-0 inputCep pl-2 border-gray-400 rounded"/>
+              <input type="text" {...register("cep")} onBlur={checkCEP} className="w-inputs p-0 border-gray-400 rounded"/>
             </div>
 
-            <div className="md:ml-5">
+            <div>
               <p>Rua *</p>
-              <input type="text" {...register("address")} className="p-0 inputRua pl-2 border-gray-400 rounded"/>
+              <input type="text" {...register("address")} className="w-inputs p-0 pl-2 border-gray-400 rounded"/>
             </div>
           </div>
 
-          <div className="md:flex justify-center md:mt-3">
-            <div className="mr-5">
+          <div className="md:grid grid-cols-4 gap-4 md:mt-5">
+            <div>
               <p>Bairro *</p>
-              <input type="text" {...register("neighborhood")} className="p-0 inputBairro pl-2 border-gray-400 rounded"/>
+              <input type="text" {...register("neighborhood")} className="w-inputs p-0 pl-2 border-gray-400 rounded"/>
             </div>
 
-            <div className="mr-5">
+            <div>
               <p>Cidade *</p>
-              <input type="text" {...register("city")} className="p-0 inputCidade w-24 pl-2 border-gray-400 rounded"/>
+              <input type="text" {...register("city")} className="w-inputs p-0 pl-2 border-gray-400 rounded"/>
             </div>
 
-            <div className="mr-5">
+            <div>
               <p>Estado *</p>
-              <input type="text" {...register("uf")} className="p-0 inputEstado w-28 pl-2 border-gray-400 rounded"/>
+              <input type="text" {...register("uf")} className="w-inputs p-0 pl-2 border-gray-400 rounded"/>
             </div>
             <div>
               <p>Número *</p>
-              <input type="text" {...register("addressNumber")} className="p-0 inputNumero pl-2 border-gray-400 rounded"/>
+              <input type="text" {...register("addressNumber")} className="w-inputs p-0 pl-2 border-gray-400 rounded"/>
             </div>
             
           </div>
